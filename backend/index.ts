71292f2cc08c27bd006f7b281b934d6ea8d73985
Lucas Ribeiro <lucasriibeiro23@gmail.com>
2024-03-01 12:16:02 -0300

    configura as variaveis de ambiente

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { Request, Response } from "express";
+import sequelize from "./src/models";
 const express = require("express");
 const app = express();
 
@@ -8,4 +11,10 @@ app.use("/", (req: Request, res: Response) => {
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+});
+
+sequelize.authenticate().then(() => {
+  console.log("Database connected");
+}).catch((err: Error) => {
+  console.log("Error: ", err);
 });
