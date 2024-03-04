ffe66a9c55ae442f8cc03f34e917efae0b78003b
lucasribeiro97 <lucasriibeiro23@gmail.com>
2024-03-04 19:18:05 -0300

    feat: implementa middleware cors

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -4,13 +4,19 @@ import cors from 'cors';
 import loginRouter from './routes/login.routes';
 import signupRouter from './routes/signup.routes';
 
+const corsOptions = {
+  origin: 'https://lexartlabs-desafio-server.vercel.app/',
+  optionsSuccessStatus: 200,
+};
+
+
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use('/login', loginRouter);
-app.use('/sign-up', signupRouter);
+app.use('/sign-up', cors(corsOptions), signupRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
