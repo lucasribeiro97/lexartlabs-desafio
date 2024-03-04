fd0f053c7c57eb690165d970d275fd21cc8e0484
lucasribeiro97 <lucasriibeiro23@gmail.com>
2024-03-04 19:24:46 -0300

    fix: usando servidor local

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -4,19 +4,13 @@ import cors from 'cors';
 import loginRouter from './routes/login.routes';
 import signupRouter from './routes/signup.routes';
 
-const corsOptions = {
-  origin: 'https://lexartlabs-desafio-server.vercel.app/',
-  optionsSuccessStatus: 200,
-};
-
-
 const app = express();
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
-app.use('/login', cors(corsOptions), loginRouter);
-app.use('/sign-up', cors(corsOptions), signupRouter);
+app.use('/login', loginRouter);
+app.use('/sign-up', signupRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
