c53e453051aae8eabdd0bcaec3c51357e585d005
lucasribeiro97 <lucasriibeiro23@gmail.com>
2024-03-04 19:09:39 -0300

    test: testando deploy

--- app/frontend/src/pages/Singup/Signup.tsx
+++ app/frontend/src/pages/Singup/Signup.tsx
@@ -4,6 +4,7 @@ import validateSignup from '../../utils/validateSignup';
 import './Signup.css';
 
 function Signup() {
+  // comentario
   const url = 'https://lexartlabs-desafio-server.vercel.app/sign-up';
   const navigate = useNavigate();
 
