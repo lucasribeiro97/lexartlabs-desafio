381dbfb3b5ca01afbf521abb03d9c8bd8a6ca40b
Lucas Ribeiro <lucasriibeiro23@gmail.com>
2024-03-04 21:49:11 -0300

    fix: configura ambiente para desenvolver localmente

--- app/backend/build/database/config/database.js
+++ app/backend/build/database/config/database.js
@@ -1,18 +1,12 @@
 "use strict";
 require("dotenv/config");
 const config = {
-    username: process.env.POSTGRES_USER || 'root',
-    password: process.env.POSTGRES_PASSWORD || '123456',
-    database: process.env.POSTGRES_DATABASE || 'verceldb',
-    host: process.env.POSTGRES_HOST || 'localhost',
-    ssl: process.env.POSTGRES_SSL === 'true',
+    username: 'postgres',
+    password: 'root',
+    database: 'lexartlabs_db',
+    host: 'localhost',
     port: 5432,
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-        }
-    }
 };
 module.exports = config;
 //# sourceMappingURL=database.js.map
--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,18 +2,12 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.POSTGRES_USER || 'root',
-  password: process.env.POSTGRES_PASSWORD || '123456',
-  database: process.env.POSTGRES_DATABASE || 'verceldb',
-  host: process.env.POSTGRES_HOST || 'localhost',
-  ssl: process.env.POSTGRES_SSL === 'true',
+  username: 'postgres',
+  password: 'root',
+  database: 'lexartlabs_db',
+  host: 'localhost',
   port: 5432,
   dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-    }
-  }
 };
 
 export = config;
