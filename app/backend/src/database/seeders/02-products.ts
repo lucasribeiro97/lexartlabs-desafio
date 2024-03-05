19b8a4e71963d4532bf0a6a879e0be8ba77acaf8
Lucas Ribeiro <lucasriibeiro23@gmail.com>
2024-03-05 09:06:01 -0300

    fix: products seeder

--- app/backend/src/database/seeders/02-products.ts
+++ app/backend/src/database/seeders/02-products.ts
@@ -7,11 +7,15 @@ export default {
         name: "Xiaomi Redmi 9",
         brand: "Xiaomi",
         model: "Redmi 9",
+        price: 200,
+        color: "Black"
       },
       {
         name: "Iphone 14 Pro",
         brand: "Apple",
         model: "14 Pro",
+        price: 1000,
+        color: "White"
       }
     ], {});
   },
